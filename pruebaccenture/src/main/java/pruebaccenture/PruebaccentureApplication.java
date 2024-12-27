2bfc87e0b594b0ffad17704fcdb89e1d5a7bae8f
Alejandroch <alejandro9.ch321@gmail.com>
2024-12-27 12:41:43 -0500

    Uploading repo to Github

---
+++ pruebaccenture/src/main/java/pruebaccenture/PruebaccentureApplication.java
@@ -0,0 +1,13 @@
+package pruebaccenture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaccentureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaccentureApplication.class, args);
+	}
+
+}
---
+++ pruebaccenture/src/test/java/pruebaccenture/PruebaccentureApplicationTests.java
@@ -0,0 +1,13 @@
+package pruebaccenture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaccentureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
