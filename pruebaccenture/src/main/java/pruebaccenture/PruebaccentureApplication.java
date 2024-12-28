f1527156780b4445d92d52994341eb99638eb098
Alejandroch <alejandro9.ch321@gmail.com>
2024-12-27 20:34:22 -0500

    Connecting to remote postgres and Dockerfile

--- pruebaccenture/src/main/java/pruebaccenture/PruebaccentureApplication.java
+++ pruebaccenture/src/main/java/pruebaccenture/PruebaccentureApplication.java
@@ -9,8 +9,9 @@ public class PruebaccentureApplication {
 
 	public static void main(String[] args) {
 		Dotenv dotenv = Dotenv.load();
-		System.setProperty("DBUSER", dotenv.get("DBUSER"));
-		System.setProperty("DBPASSWORD", dotenv.get("DBPASSWORD"));
+		System.setProperty("DB_CONNECTION_URL", dotenv.get("DB_CONNECTION_URL"));
+		System.setProperty("DB_USER", dotenv.get("DB_USER"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
 		SpringApplication.run(PruebaccentureApplication.class, args);
 	}
 
